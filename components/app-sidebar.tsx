70aed52f4206eb4100adf181746d53768367c21f
txdeno <txdeno@gmail.com>
2025-03-10 14:51:53 +0700

    Update app sidebar logo with new branding image

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -34,9 +34,14 @@ export function AppSidebar({ user }: { user: User | undefined }) {
               }}
               className="flex flex-row gap-3 items-center"
             >
-              <span className="text-lg font-semibold px-2 hover:bg-muted rounded-md cursor-pointer">
+              {/* <span className="text-lg font-semibold px-2 hover:bg-muted rounded-md cursor-pointer">
                 Chatbot
-              </span>
+              </span> */}
+              <img
+                className="h-[77px] aspect-video object-contain"
+                src="/images/logo-typo.jpg"
+                alt="logo"
+              />
             </Link>
             <Tooltip>
               <TooltipTrigger asChild>
